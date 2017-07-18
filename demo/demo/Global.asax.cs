b83bf9fb95a0e488fc97c4d1369d18744a0b7c7c
zero <shanghouxiong@live.cn>
2017-07-18 17:56:41 +0800

    update

--- demo/demo/Global.asax.cs
+++ demo/demo/Global.asax.cs
@@ -13,11 +13,11 @@ namespace demo
     {
         protected void Application_Start()
         {
-           // XmlConfigurator.Configure();
+            // XmlConfigurator.Configure();
             //应用启动时加载log4net配置，还有别一种方法，
             //FileSystemWatcher监听，如果watch为true
             //相应的AssemblyInfo中添加[assembly: XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
-            var logCfg = new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config");
+            FileInfo logCfg = new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config");
             XmlConfigurator.ConfigureAndWatch(logCfg);
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
